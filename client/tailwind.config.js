e7ca3711602029c756a96d52c5688db521e47e47
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-07-24 13:58:46 +0200

    styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#F5385D'
+        primary: "#F5385D",
       },
     },
   },
   plugins: [
-    
+    function ({ addUtilities }) {
+      addUtilities({
+        ".hide-scrollbars": {
+          overflow: "auto",
+        },
+        ".hide-scrollbars::-webkit-scrollbar": {
+          display: "none",
+        },
+        ".hide-scrollbars": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+      });
+    },
   ],
-}
-
+};
