5586b981326e7b5952735d1a3df801225ef0653d
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-07-24 11:36:35 +0200

    ./.render-build.sh

---
+++ .render-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# Install dependencies
+npm install
+npm run install-client
+
+# Ensure concurrently is executable
+chmod +x ./node_modules/.bin/concurrently
+
+# Build the client
+npm run build-client
